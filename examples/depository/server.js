dbb65f4cd739f5c9df4a7aef514b82776c161ab5
Adam Saleh <asaleh@redhat.com>
2017-04-11 17:24:59 +0200

    Added new example

---
+++ examples/depository/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const PouchDB = require('pouchdb');
+
+const db = new PouchDB('./database');
+
+const app = express();
+const router = express.Router();
+
+app.use('/locker', router);
+app.listen(8000, function () {
+  console.log('Listening at http://localhost:8000');
+});
+
+function wrap (responder) {
+  return async (req, res) => {
+    try {
+      await responder(req, res);
+    } catch (exception) {
+      res.status(500).json(exception);
+    }
+  };
+}
+
+router.post('/', wrap(async (req, res) => {
+  const dbResponse = await db.post({'_attachments': {}});
+  res.status(200).json(dbResponse);
+}));
+
+router.get('/:id/items', wrap(async (req, res) => {
+  const id = req.params.id;
+  const dbResponse = await db.get(id);
+  const attachments = Object.keys(dbResponse._attachments);
+  res.status(200).json(attachments);
+}));
+
+router.delete('/:id', wrap(async (req, res) => {
+  var id = req.params.id;
+  const doc = await db.get(id);
+  const dbResponse = await db.remove(id, doc._rev);
+  res.json(dbResponse);
+}));
+
+function getReqBuffer (req) {
+  return new Promise((resolve, reject) => {
+    const bufs = [];
+    req.on('data', d => bufs.push(d));
+    req.on('end', () => resolve(Buffer.concat(bufs)));
+    req.on('err', e => reject(e));
+  });
+}
+
+router.post('/:id/item/:name', wrap(async (req, res) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  const buffer = await getReqBuffer(req);
+  const doc = await db.get(id);
+  doc['_attachments'][name] = {
+    'content_type': 'application/octet-stream',
+    'data': buffer
+  };
+  const response = await db.put(doc);
+  res.json(response);
+}));
+
+router.get('/:id/item/:name', wrap(async (req, res) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  const response = await db.getAttachment(id, name);
+  res.type('application/octet-stream');
+  res.end(response, 'binary');
+}));
